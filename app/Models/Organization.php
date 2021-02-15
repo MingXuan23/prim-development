624eee3d4fe3863c914feaa1ede70073d7acdd7e
hafizj98 <hafizjamil265@gmail.com>
2021-02-15 11:52:50 +0800

    add relation to table user and organization_role

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,12 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Models\OrganizationRole;
 
 class Organization extends Model
 {
-    //
-
     protected $fillable = ['nama', 'code', 'email', 'telno', 'address', 'postcode', 'state'];
 
     public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'organization_user');
+    }
+
+    public function organizationRole()
+    {
+        return $this->belongsToMany(OrganizationRole::class, 'organization_user', 'role_id');
+    }
 }
