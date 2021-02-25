e5dd91101003b7f9130e86c6cfb0d0d251915f6a
afiq101 <afiq101@users.noreply.github.com>
2021-02-25 18:05:55 +0800

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,16 +17,4 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
-            URL::forceScheme('https');
-        }
-    }
 }
