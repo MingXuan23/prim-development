f7d669b6f68ac6dd41e63bc89f68231c2a4df0f2
hafizj98 <hafizjamil265@gmail.com>
2021-03-18 12:19:50 +0800

    update dashboard backend

--- routes/web.php
+++ routes/web.php
@@ -93,6 +93,5 @@ Route::group(['prefix' => 'notification'], function () {
 // Route::get('/offline', 'HomeController@pwaOffline');
 
 Route::group(['prefix' => 'dashboard'], function () {
-    Route::get('/donor', 'HomeController@getTotalDonorDashboard')->name('donor');
-    Route::get('/donation', 'HomeController@getTotalDonationDashboard')->name('donation');
+    Route::get('/item', 'HomeController@getDashboardItem')->name('dashboard.item');
 });
