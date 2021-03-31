7cda605eb541ac4ec15c1abf39f9352135579f46
hafizj98 <hafizjamil265@gmail.com>
2021-03-31 23:45:27 +0800

    minor fix

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master-without-nav')
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+@endsection
+
+
+@section('script')
+<!-- Peity chart-->
+<script src="{{ URL::asset('assets/libs/peity/peity.min.js')}}"></script>
+
+{{-- <script src="{{ URL::asset('assets/js/pages/dashboard.init.js')}}"></script> --}}
+@endsection
