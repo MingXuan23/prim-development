61e913c443886758768294572447f8952dcd1dcc
hishamudin98 <hishamudin.ali@gmail.com>
2021-04-02 09:46:08 +0800

    add activity features

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     //
+    protected $fillable = ['name', 'description', 'date_created', 'date_start', 'date_end', 'status', 'organization_id'];
+
+    public $timestamps = false;
+
+    protected $dates = ['deleted_at'];
+
+    public function organization()
+    {
+        return $this->hasMany(Organization::class);
+    }
+
 }
--- database/migrations/2021_04_01_154913_create_activities_table.php
+++ database/migrations/2021_04_01_154913_create_activities_table.php
@@ -17,11 +17,15 @@ class CreateActivitiesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name')->nullable();
             $table->string('description')->nullable();
+            $table->string('picture')->nullable();
+            $table->date('date_created')->nullable();
             $table->date('date_start')->nullable();
             $table->date('date_end')->nullable();
             $table->string('status')->nullable();
             $table->unsignedBigInteger('organization_id');
             $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
+            $table->softDeletes();
+
         });
     }
 
@@ -35,6 +39,7 @@ class CreateActivitiesTable extends Migration
         Schema::dropIfExists('activities', function (Blueprint $table)
         {
             $table->drop('organization_id');
+            $table->dropSoftDeletes();
 
         });
     }
