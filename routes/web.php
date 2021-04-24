38525a918ebff67cc50429cd10feb95a503f1ccb
hafizj98 <hafizjamil265@gmail.com>
2021-04-24 17:42:27 +0800

    fix route for landing page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 Route::get('/form', 'HomeController@form');
 // Route::get('/school', 'FeeController@index');
 // Route::get('/school', 'SchoolController@index');
