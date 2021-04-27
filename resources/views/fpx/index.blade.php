81e3e4e85beeeaa0943b7cb94e1f9a3394f3d593
hafizj98 <hafizjamil265@gmail.com>
2021-04-28 00:08:14 +0800

    fix button

--- resources/views/fpx/index.blade.php
+++ resources/views/fpx/index.blade.php
@@ -14,8 +14,8 @@
                                         <tr>
                                             <td height="164" align="center" class="main"><b>Payment Method via FPX</b>
                                                 <p>&nbsp;</p>
-                                                {{-- <input type="submit" style="cursor:hand" class="btn btn-primary" onclick="pay()"
-                                                    value="Click to Pay" /> --}}
+                                                <input type="submit" style="cursor:hand" class="btn btn-primary" onclick="pay()"
+                                                    value="Click to Pay" />
                                                 <p> <img src="assets/images/FPXButton.PNG" border="2" /></p>
                                                 <p class="main">&nbsp;</p>
                                                 <p class="main"><strong>* You must have Internet Banking Account in order to
@@ -71,11 +71,11 @@
     // console.log('hihi');
     var values = $("#form1").serialize();
     
-    function redirectFPX() {
-        $('#form1').submit();
-    }
+    // function redirectFPX() {
+    //     $('#form1').submit();
+    // }
     // $('#form1').submit();
-    setTimeout("redirectFPX();",3000);
+    // setTimeout("redirectFPX();",3000);
 
    
 
