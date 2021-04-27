fc46e4c104d430a6fac2e9a81c08932968258373
ahmdraziq <danishtraver@gmail.com>
2021-04-28 00:00:01 +0800

    a

--- resources/views/fpx/tStatus.blade.php
+++ resources/views/fpx/tStatus.blade.php
@@ -32,6 +32,8 @@ $fpx_buyerIban="";
 $fpx_productDesc="SampleProduct";
 $fpx_version="6.0";
 
+echo $fpx_msgType;
+
 // $data=$fpx_buyerBankBranch."|".$fpx_buyerBankId."|".$fpx_buyerIban."|".$fpx_buyerId."|".$fpx_buyerName."|".$fpx_creditAuthCode."|".$fpx_creditAuthNo."|".$fpx_debitAuthCode."|".$fpx_debitAuthNo."|".$fpx_fpxTxnId."|".$fpx_fpxTxnTime."|".$fpx_makerName."|".$fpx_msgToken."|".$fpx_msgType."|".$fpx_sellerExId."|".$fpx_sellerExOrderNo."|".$fpx_sellerId."|".$fpx_sellerOrderNo."|".$fpx_sellerTxnTime."|".$fpx_txnAmount."|".$fpx_txnCurrency;
 
 extract($_POST);
@@ -69,8 +71,6 @@ try{
 //url-ify the data for the POST
 foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 rtrim($fields_string, '&');
-
-echo $fields_string;
 
 //open connection
 $ch = curl_init();
