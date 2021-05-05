d2aee25031bbe10a1d6bf1e5020328029a4f560f
ahmdraziq <danishtraver@gmail.com>
2021-05-05 16:27:39 +0800

    ae fix

--- resources/views/fpx/tStatus.blade.php
+++ resources/views/fpx/tStatus.blade.php
@@ -23,7 +23,8 @@ $fpx_sellerBankCode="01";
 $fpx_txnCurrency="MYR";
 $fpx_txnAmount=$request->fpx_txnAmount;
 $fpx_buyerEmail=$user->email;
-$fpx_checkSum=$user->fpx_checksum;
+// $fpx_checkSum=$user->fpx_checksum;
+$fpx_checkSum="";
 $fpx_buyerName=$user->username;
 $fpx_buyerBankId=$request->fpx_buyerBankId;
 $fpx_buyerAccNo="";
@@ -35,12 +36,12 @@ $fpx_productDesc=explode("_", $request->fpx_sellerExOrderNo)[0];
 $fpx_version="6.0";
 
 // dd($fpx_checkSum);
-// $data=$fpx_buyerBankBranch."|".$fpx_buyerBankId."|".$fpx_buyerIban."|".$fpx_buyerId."|".$fpx_buyerName."|".$fpx_creditAuthCode."|".$fpx_creditAuthNo."|".$fpx_debitAuthCode."|".$fpx_debitAuthNo."|".$fpx_fpxTxnId."|".$fpx_fpxTxnTime."|".$fpx_makerName."|".$fpx_msgToken."|".$fpx_msgType."|".$fpx_sellerExId."|".$fpx_sellerExOrderNo."|".$fpx_sellerId."|".$fpx_sellerOrderNo."|".$fpx_sellerTxnTime."|".$fpx_txnAmount."|".$fpx_txnCurrency;
+$data=$fpx_buyerBankBranch."|".$fpx_buyerBankId."|".$fpx_buyerIban."|".$fpx_buyerId."|".$fpx_buyerName."|".$fpx_creditAuthCode."|".$fpx_creditAuthNo."|".$fpx_debitAuthCode."|".$fpx_debitAuthNo."|".$fpx_fpxTxnId."|".$fpx_fpxTxnTime."|".$fpx_makerName."|".$fpx_msgToken."|".$fpx_msgType."|".$fpx_sellerExId."|".$fpx_sellerExOrderNo."|".$fpx_sellerId."|".$fpx_sellerOrderNo."|".$fpx_sellerTxnTime."|".$fpx_txnAmount."|".$fpx_txnCurrency;
 
-// $priv_key = getenv('FPX_KEY');
-// $pkeyid = openssl_get_privatekey($priv_key, null);
-// openssl_sign($data, $binary_signature, $pkeyid, OPENSSL_ALGO_SHA1);
-// $fpx_checkSum = strtoupper(bin2hex($binary_signature));
+$priv_key = getenv('FPX_KEY');
+$pkeyid = openssl_get_privatekey($priv_key, null);
+openssl_sign($data, $binary_signature, $pkeyid, OPENSSL_ALGO_SHA1);
+$fpx_checkSum = strtoupper(bin2hex($binary_signature));
 
 $fields_string="";
 
