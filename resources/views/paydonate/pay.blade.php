e1726d80edbdccdba229b43330651b505ba217d9
hafizj98 <hafizjamil265@gmail.com>
2021-05-09 23:50:00 +0800

    fix storage bug

--- resources/views/paydonate/pay.blade.php
+++ resources/views/paydonate/pay.blade.php
@@ -22,7 +22,7 @@
     <div class="col-lg-6 ">
         <div class="card h-100">
             <div class="card-body">
-                <img class="img-fluid card-img" src="{{ URL::asset('storage/donation-poster/'.$donation->donation_poster) }}"
+                <img class="img-fluid card-img" src="{{ URL::asset('/donation-poster/'.$donation->donation_poster) }}"
                                     alt="Image Not Available">
             </div>
         </div>    
