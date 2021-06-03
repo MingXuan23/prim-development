5c4ed4936101058dd074514de6c515cdb7c156c7
Hisham <hishamudin.ali@gmail.com>
2021-06-03 16:13:32 +0800

    minor changes

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -20,16 +20,6 @@
     <div class="col-md-12">
         <div class="card card-primary">
 
-            @if(count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                    <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-
             {{csrf_field()}}
             <div class="card-body">
 
