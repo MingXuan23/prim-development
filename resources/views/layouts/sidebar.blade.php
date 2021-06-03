2c513c66da4076950ad3f6f0772be66cca8efdcb
Hisham <hishamudin.ali@gmail.com>
2021-06-03 16:13:17 +0800

    fix navigate dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
                 <li class="menu-title">Main</li>
 
                 <li>
-                    <a href="/" class="waves-effect">
+                    <a href="/home" class="waves-effect">
                         <i class="ti-home"></i>
                         {{-- <span class="badge badge-pill badge-primary float-right">2</span> --}}
                         <span>Dashboard</span>
