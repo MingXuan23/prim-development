07b192d473bb14d3ef68f136c38ea0d0a5d9c345
Hisham <hishamudin.ali@gmail.com>
2021-06-08 21:03:58 +0800

    add seed type organization

--- database/seeds/TypeOrganizationsTableSeeder.php
+++ database/seeds/TypeOrganizationsTableSeeder.php
@@ -16,21 +16,26 @@ class TypeOrganizationsTableSeeder extends Seeder
             0 =>
             array(
                 "id" => 1,
-                "nama" => "SK/SJK",
+                "nama" => "SK /SJK",
             ),
             1 =>
             array(
                 "id" => 2,
-                "nama" => "SRA/SRAI",
+                "nama" => "SRA /SRAI",
             ),
             2 =>
             array(
                 "id" => 3,
-                "nama" => "Masjid",
+                "nama" => "SMK /SMJK",
             ),
             3 =>
             array(
                 "id" => 4,
+                "nama" => "Masjid",
+            ),
+            4 =>
+            array(
+                "id" => 5,
                 "nama" => "NGO",
             ),
         ));
