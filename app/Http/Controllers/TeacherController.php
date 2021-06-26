ed779934c18e4e9deaac4cfbde1b8bd12e951111
Hisham <hishamudin.ali@gmail.com>
2021-06-26 20:14:03 +0800

    teacher fix

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\models\Teacher;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
