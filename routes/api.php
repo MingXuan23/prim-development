12952c253191010eb75f323906b0724a23d4edc4
Hisham <hishamudin.ali@gmail.com>
2021-06-29 15:21:04 +0800

    add api dev transactions

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::post('devtrans', 'PayController@devtrans')->name('devtrans');
+
