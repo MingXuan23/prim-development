4a41981eff2ff27bce17694dd807be2f0219b297
Hisham <hishamudin.ali@gmail.com>
2021-07-05 16:38:41 +0800

    receipt changes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,7 @@
         <link href="{{ URL::asset('assets/css/bootstrap-dark.min.css')}}" id="bootstrap-dark" rel="stylesheet" type="text/css" />
         <link href="{{ URL::asset('assets/css/bootstrap.min.css')}}" id="bootstrap-light" rel="stylesheet" type="text/css" />
         <link href="{{ URL::asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ URL::asset('assets/css/app-rtl.min.css')}}" id="app-rtl" rel="stylesheet" type="text/css" />
+        {{-- <link href="{{ URL::asset('assets/css/app-rtl.min.css')}}" id="app-rtl" rel="stylesheet" type="text/css" /> --}}
         <link href="{{ URL::asset('assets/css/app-dark.min.css')}}" id="app-dark" rel="stylesheet" type="text/css" />
         <link href="{{ URL::asset('assets/css/app.min.css')}}" id="app-light" rel="stylesheet" type="text/css" />
         <link href="{{ URL::asset('assets/css/style.css')}}" id="app-light" rel="stylesheet" type="text/css" />
