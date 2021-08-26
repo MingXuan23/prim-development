c8c5336957e8c1441655095b4eca6234ae7ee579
MNajmi540 <muhdnajmi540@gmail.com>
2021-08-26 21:06:35 +0800

    comment

--- database/migrations/2020_12_17_073444_create_donations_table.php
+++ database/migrations/2020_12_17_073444_create_donations_table.php
@@ -25,7 +25,6 @@ class CreateDonationsTable extends Migration
             $table->string('tax_payer')->nullable();
             $table->float('total_tax')->nullable();
             $table->string('donation_poster')->nullable();
-            $table->string('donation_type')->nullable();
         });
     }
 
