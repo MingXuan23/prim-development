8ca858a969f6a3c7d8f968ac9a7782809516ee51
Stephen-Sim <simshansiong2002@gmail.com>
2021-12-21 11:48:37 +0800

    fix landing page

--- resources/views/landing-page/head.blade.php
+++ resources/views/landing-page/head.blade.php
@@ -121,13 +121,12 @@
         width: auto;
         height: 520px;
         flex-grow: 1;
-        
-        z-index: -1;
     }
-
+    
     .header-area {
         padding: 140px 0 0 0;
         background-color: #fff;
         position: relative;
+        z-index: -1;
     }
 </style>
