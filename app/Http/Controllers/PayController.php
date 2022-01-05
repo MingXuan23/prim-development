7d74b05dcd0f35385551fbdcf6a35c5d74783950
Stephen-Sim <simshansiong2002@gmail.com>
2022-01-05 16:33:59 +0800

    change seller order no

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -395,7 +395,7 @@ class PayController extends AppBaseController
             }
 
             $fpx_sellerExOrderNo = $request->desc . "_" . $request->d_code . "_" . date('YmdHis') . "_" . $request->o_id;
-            $fpx_sellerOrderNo  =  $request->o_id . "_" . "PRIM" . date('YmdHis') . rand(10000, 99999);
+            $fpx_sellerOrderNo  =  "PRIM" . $request->o_id . "_" .  date('YmdHis') . rand(10000, 99999);
             $fpx_sellerExId     = config('app.env') == 'production' ? "EX00011125" : "EX00012323";
 
             $fpx_sellerId       = config('app.env') == 'production' ? $organization->seller_id : "SE00013841";
