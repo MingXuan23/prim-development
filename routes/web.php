e4692da1c14d0cd4b7d472ab777f8d25c54ee793
Stephen-Sim <simshansiong2002@gmail.com>
2022-04-05 11:46:11 +0800

    remove register route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index');
 Route::get('/form', 'HomeController@form');
