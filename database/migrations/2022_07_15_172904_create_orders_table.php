97e7dc4268fb387126cf82f7d496ff712a92e34e
kimie7 <hakiminajibecr77@gmail.com>
2022-07-25 10:50:01 +0800

    update order's table

--- database/migrations/2022_07_15_172904_create_orders_table.php
+++ database/migrations/2022_07_15_172904_create_orders_table.php
@@ -18,12 +18,19 @@ class CreateOrdersTable extends Migration
             $table->string('delivery_status')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('organ_id');
+            $table->unsignedBigInteger('dish_available_id');
             $table->unsignedBigInteger('transaction_id')->nullable();
+            $table->double('delivery_latitude')->nullable();
+            $table->double('delivery_longitude')->nullable();
+            $table->string('order_description')->nullable();
+            $table->integer('PhoneNumber')->nullable();
+            $table->string('username')->nullable();
             $table->timestamps();
             
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('organ_id')->references('id')->on('organizations')->onDelete('cascade');
             $table->foreign('transaction_id')->nullable()->references('id')->on('transactions')->onDelete('cascade');
+            $table->foreign('dish_available_id')->references('id')->on('dish_available')->onDelete('cascade');
         });
     }
 
