61fc67eb747eded80c715008802cee6c16100668
kimie7 <hakiminajibecr77@gmail.com>
2022-07-28 12:56:16 +0800

    order desc new file

--- database/migrations/2022_07_28_125400_add_desc_column_org.php
+++ database/migrations/2022_07_28_125400_add_desc_column_org.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsToOrganization extends Migration
+class AddDescColumnOrg extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddColumnsToOrganization extends Migration
     public function up()
     {
         Schema::table('organizations', function (Blueprint $table) {
-            $table->string('district');
+            $table->string('description');
         });
     }
 
