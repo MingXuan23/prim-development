2a175cfb0a82783405e1e7a797b68fd999094bd4
siowzheyi <siowzheyi@gmail.com>
2022-08-08 09:49:51 +0800

    micole add column

---
+++ database/migrations/2022_08_08_093730_add_columns_to_class_student_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToClassStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('class_student', function (Blueprint $table) {
+            //
+            $table->integer('outing_status');
+            $table->integer('blacklist')->nullable();
+            $table->integer('start_date_time')->nullable();
+            $table->integer('end_date_time')->nullable();
+            $table->bigInteger('dorm_id')->unsigned()->nullable()->after('end_date_time');
+            $table->foreign('dorm_id')->references('id')->on('dorms')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('class_student', function (Blueprint $table) {
+            //
+        });
+    }
+}
