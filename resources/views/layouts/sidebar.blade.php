d378a8e9052694ecb3e8b65c6269d27691c0d625
9979Gyu <yuqin1161@gmail.com>
2022-08-12 14:33:23 +0800

    CHANGES IN SIDEBAR

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -173,6 +173,15 @@
                                 </li>
                                 @endrole
                                 <!--fas fa-bed-->
+
+                                @role('Superadmin|Pentadbir|Guru|Warden')
+                                <li>
+                                    <a href="{{ route('dorm.indexResident') }}" class=" waves-effect">
+                                        <i class="fas fa-bus"></i>
+                                        <span>Resident</span>
+                                    </a>
+                                </li>
+                                @endrole
                             </ul>
                         </li>
                         @endrole
