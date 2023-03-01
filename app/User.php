7e84acf084f321ad08cc71371fcaa1ad13eb262e
Stephen-Sim <simshansiong2002@gmail.com>
2023-03-01 22:28:07 +0800

    yuran api

--- app/User.php
+++ app/User.php
@@ -12,6 +12,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
