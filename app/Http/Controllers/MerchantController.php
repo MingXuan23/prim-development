60361a2711fd67323f896fe66f9c7a8f103e7a21
Fei0716 <ahfeiahfeiahfei@gmail.com>
2023-04-11 21:53:03 +0800

    fei trying

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
-
+//made by Fei
 //made by bingxian
 class MerchantController extends Controller
 {
