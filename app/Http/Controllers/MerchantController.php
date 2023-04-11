9289169158c0a7351ef29bc89a3aec2a0fcdf953
bingxian0701 <bingxian030701@gmail.com>
2023-04-11 21:34:20 +0800

    Bingxian_code

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 
+//made by bingxian
 class MerchantController extends Controller
 {
     public function merchantList()
