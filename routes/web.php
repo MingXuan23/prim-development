ecb051167d940dd1e61c7e65651f3249ee4aa9e2
Stephen-Sim <simshansiong2002@gmail.com>
2023-04-19 11:31:23 +0800

    add cron job

--- routes/web.php
+++ routes/web.php
@@ -406,7 +406,7 @@ Route::group(['prefix' => 'dashboard'], function () {
 
 Route::group(['prefix' => 'fpx'], function () {
     Route::get('/getBankList', 'FPXController@getBankList')->name('fpx.bank_list');
-    Route::post('/requery', 'FPXController@requery')->name('requery');
+    Route::get('/requery', 'FPXController@requery')->name('requery');
 });
 
 Route::get('/receipt', 'PayController@showReceipt');
