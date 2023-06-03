c7ea2df73db78fe7d545fbf91200dee1f571101f
MingXuan23 <mxchuah23@gmail.com>
2023-06-03 21:14:58 +0800

    change seller id in kooperasi

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -499,7 +499,7 @@ class PayController extends AppBaseController
             $fpx_sellerOrderNo  = "KOPPRIM" . date('YmdHis') . rand(10000, 99999);
 
             $fpx_sellerExId     = config('app.env') == 'production' ? "EX00011125" : "EX00012323";
-            $fpx_sellerId       = config('app.env') == 'production' ? $ficts_seller_id : "SE00013841";
+            $fpx_sellerId       = config('app.env') == 'production' ? $organization->seller_id : "SE00013841";
         }
 
         $fpx_msgType        = "AR";
