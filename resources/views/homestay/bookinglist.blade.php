04145e1a146018ee68e4f2deb96119236aa5302c
Amir Hadi <aamirhdi5@gmail.com>
2023-09-22 03:15:29 +0800

    test

--- resources/views/homestay/bookinglist.blade.php
+++ resources/views/homestay/bookinglist.blade.php
@@ -21,6 +21,16 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-body">
+
+            @if(count($errors) > 0)
+      <div class="alert alert-danger">
+        <ul>
+          @foreach($errors->all() as $error)
+          <li>{{$error}}</li>
+          @endforeach
+        </ul>
+      </div>
+      @endif
             @if(Session::has('success'))
             <div class="alert alert-success">
               <p>{{ Session::get('success') }}</p>
--- resources/views/homestay/setpromotion.blade.php
+++ resources/views/homestay/setpromotion.blade.php
@@ -23,6 +23,16 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card card-primary">
+
+        @if(count($errors) > 0)
+      <div class="alert alert-danger">
+        <ul>
+          @foreach($errors->all() as $error)
+          <li>{{$error}}</li>
+          @endforeach
+        </ul>
+      </div>
+      @endif
             
         @if(Session::has('success'))
             <div class="alert alert-success">
