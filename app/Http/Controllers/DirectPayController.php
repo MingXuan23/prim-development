ba9b31fab673cfada6b981515ee5a5ad221a999b
MingXuan23 <mxchuah23@gmail.com>
2024-03-09 15:19:55 +0800

    check transaction

--- app/Http/Controllers/DirectPayController.php
+++ app/Http/Controllers/DirectPayController.php
@@ -1676,6 +1676,12 @@ class DirectPayController extends Controller
         // Close cURL session
         curl_close($ch);
         $resultArray = json_decode($response, true);
+        if (!isset($resultArray['fpx_DebitAuthCode']))
+                {
+                    dd($resultArray,"here");
+                }
+                dd($resultArray,"here");
+
         return $resultArray ;
     }
 
