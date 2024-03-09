0cba05f6bbd2c3663ed698f95f0ffdd4917b4448
MingXuan23 <mxchuah23@gmail.com>
2024-03-09 17:00:22 +0800

    update cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('transactionstatus:cron')->everyFourHours();
-        $schedule->command('transactionstatus:cron')->cron('5 0 * * *');
+        $schedule->command('transactionstatus:cron')->everyMinute();
         $schedule->command('fees:reminder')->dailyAt('09:00');
         $schedule->command('balances:reminder')->dailyAt('09:00');
     }
