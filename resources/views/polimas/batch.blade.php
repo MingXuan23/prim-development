64cbc755909edb4d80c8189cb47c1d17c13a8884
EugeneHZC <momoeugenehan@gmail.com>
2025-11-18 21:42:23 +0800

    Fixed a minor issue

--- resources/views/polimas/batch.blade.php
+++ resources/views/polimas/batch.blade.php
@@ -1,81 +1,81 @@
 @extends('layouts.master')
 
 @section('css')
-<link href="{{ URL::asset('assets/libs/chartist/chartist.min.css')}}" rel="stylesheet" type="text/css" />
-@include('layouts.datatable')
+    <link href="{{ URL::asset('assets/libs/chartist/chartist.min.css')}}" rel="stylesheet" type="text/css" />
+    @include('layouts.datatable')
 
 @endsection
 
 @section('content')
-<div class="row align-items-center">
-    <div class="col-sm-6">
-        <div class="page-title-box">
-            <h4 class="font-size-18">Batch</h4>
+    <div class="row align-items-center">
+        <div class="col-sm-6">
+            <div class="page-title-box">
+                <h4 class="font-size-18">Batch</h4>
+            </div>
         </div>
     </div>
-</div>
-<div class="row">
-    {{csrf_field()}}
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="form-group">
-                    <label>Nama Organisasi</label>
-                    <select name="organization" id="organization" class="form-control">
-                        <option value="{{ $organization->id }}">{{ $organization->nama }}</option>
-                    </select>
+    <div class="row">
+        {{csrf_field()}}
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label>Nama Organisasi</label>
+                        <select name="organization" id="organization" class="form-control">
+                            <option value="{{ $organization->id }}">{{ $organization->nama }}</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table id="classesTable" class="table table-bordered table-striped dt-responsive nowrap"
-                        style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                        <thead>
-                            <tr style="text-align:center">
-                                <th>No. </th>
-                                <th>Nama Kelas</th>
-                                <th>Bilangan Pelajar</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="classesTable" class="table table-bordered table-striped dt-responsive nowrap"
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr style="text-align:center">
+                                    <th>No. </th>
+                                    <th>Nama Kelas</th>
+                                    <th>Bilangan Pelajar</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    </div>
+    </div>
+    </div>
 @endsection
 
 
 @section('script')
-<!-- Peity chart-->
-<script src="{{ URL::asset('assets/libs/peity/peity.min.js')}}"></script>
+    <!-- Peity chart-->
+    <script src="{{ URL::asset('assets/libs/peity/peity.min.js')}}"></script>
 
-<!-- Plugin Js-->
-<script src="{{ URL::asset('assets/libs/chartist/chartist.min.js')}}"></script>
+    <!-- Plugin Js-->
+    <script src="{{ URL::asset('assets/libs/chartist/chartist.min.js')}}"></script>
 
-<script src="{{ URL::asset('assets/js/pages/dashboard.init.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/pages/dashboard.init.js')}}"></script>
 
 
-<script>
-    $(document).ready(function() {
-        $(function(){
-            classesTable = $('#classesTable').DataTable({
+    <script>
+        $(document).ready(function () {
+            $(function () {
+                classesTable = $('#classesTable').DataTable({
                     processing: true,
                     serverSide: true,
                     ajax: {
                         url: "{{ route('polimas.batch.getBatchDataTable') }}",
                         type: 'GET',
-  
+
                     },
                     'columnDefs': [{
                         "targets": [0], // your case first column
                         "className": "text-center",
                         "width": "2%"
-                    },{
-                        "targets": [1,2], // your case first column
+                    }, {
+                        "targets": [1, 2], // your case first column
                         "className": "text-center",
                     },],
                     order: [
@@ -90,24 +90,24 @@
                         }
                     }, {
                         data: 'cnama',
-                        name: 'cnama'
+                        name: 'nama'
                     }, {
                         data: 'totalstudent',
                         name: 'totalstudent',
                         orderable: false,
                         searchable: false,
                     }]
+                });
             });
-        });
 
-        // csrf token for ajax
-        $.ajaxSetup({
+            // csrf token for ajax
+            $.ajaxSetup({
                 headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
         });
-  
-    });
-</script>
+    </script>
 
 @endsection
