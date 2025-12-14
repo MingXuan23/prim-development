4abdad4744a11c56789b517613d7c0b50a993be9
EugeneHZC <momoeugenehan@gmail.com>
2025-12-14 16:57:23 +0800

    Fixed duplicate names appearing when export excel

--- app/Exports/ExportYuranStatus.php
+++ app/Exports/ExportYuranStatus.php
@@ -48,6 +48,8 @@ class ExportYuranStatus implements WithMultipleSheets
                 ->leftJoin('class_organization as co', 'co.id', 'cs.organclass_id')
                 ->leftJoin('classes as c', 'c.id', 'co.class_id')
                 ->where('ou.organization_id', $orgId)
+                ->where('cs.status', 1)
+                ->where('c.status', 1)
                 ->select('s.nama', 'c.nama as nama_kelas', 's.gender', 'ou.user_id')
                 ->orderBy('c.nama')
                 ->orderBy('s.nama')
@@ -57,7 +59,6 @@ class ExportYuranStatus implements WithMultipleSheets
                 $studentItem = $student->where('user_id', $fee->user_id)->first();
                 return (object) array_merge((array) $studentItem, (array) $fee);
             });
-
 
             foreach ($data as &$item) {
                 unset($item->user_id);
