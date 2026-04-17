b990d3ef3e42f90c1ec7f181c192b149f6810dac
EugeneHZC <momoeugenehan@gmail.com>
2026-04-17 16:33:24 +0800

    refactored auth handoff token

--- app/Http/Controllers/AuthHandoffController.php
+++ app/Http/Controllers/AuthHandoffController.php
@@ -13,11 +13,13 @@ class AuthHandoffController extends Controller
     public function getHandoffToken(Request $request)
     {
         $request->validate([
-            "redirect_url" => "required",
+            "donation_id" => "required",
+            "user_token" => "required",
+            "desc" => "required"
         ]);
 
         $user = DB::table("user_token")
-            ->where('remember_token', '=', $request->header('user_token'))
+            ->where('remember_token', '=', $request->get('user_token'))
             ->first();
 
         if ($user == null) {
@@ -30,7 +32,8 @@ class AuthHandoffController extends Controller
 
         $payload = [
             "user_id" => $user->id,
-            "redirect_url" => $request->get('redirect_url')
+            "donation_id" => $request->get('donation_id'),
+            "desc" => $request->get("desc")
         ];
 
         Cache::put($cacheKey, $payload, 60);
@@ -49,8 +52,14 @@ class AuthHandoffController extends Controller
 
         Auth::loginUsingId($payload["user_id"]);
 
-        $url = $payload['redirect_url'];
+        $donation = DB::table("donations")
+            ->where("id", "=", $payload["donation_id"])
+            ->first();
 
-        return redirect("sumbangan/$url");
+        if ($payload["desc"] == "Derma Tanpa Nama") {
+            return redirect()->route('ANONdonate', ['link' => $donation->url]);
+        } else {
+            return redirect()->route('URLdonate', ['link' => $donation->url]);
+        }
     }
 }
