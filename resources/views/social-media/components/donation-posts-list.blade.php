706ff8d33463aca4a1f23330ca38724005877593
EugeneHZC <eugenehzc06@gmail.com>
2026-08-17 09:22:05 +0800

    Added text to say no results when there is no donations

--- resources/views/social-media/components/donation-posts-list.blade.php
+++ resources/views/social-media/components/donation-posts-list.blade.php
@@ -1,3 +1,7 @@
+@if ($donations->count() == 0)
+    <p>Tiada derma.</p>
+@endif
+
 @foreach ($donations as $donation)
     @include('social-media.components.donation-card', ['donation' => $donation])
 @endforeach
